18c208b333150a6d9c6ab2186b1a91d45fad6bfd
keven-bardales <122845766+keven-bardales@users.noreply.github.com>
2023-10-17 18:22:09 -0600

    feat(test): main testing

--- __test__/home.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('Home Page', () => {
-  it('should render the greeting', () => {
-    cy.visit('http://localhost:3000');
-    cy.contains('Get started by editing app/page.tsx');
-  });
-});
