f696efc84c2f41d6a126b82c46c45cd1122c87f4
keven-bardales <122845766+keven-bardales@users.noreply.github.com>
2023-10-17 18:23:32 -0600

    feat(update): update

---
+++ _next/static/91F_g7FUHspWQiaXT91km/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-4e32f5bf9f8e42bc.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
